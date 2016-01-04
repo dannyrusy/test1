2bb186d32c0d33380615bfe27675d6b2a934de8e
Daniele Peruzzi <daniele.peruzzi@globalbase.it>
2016-01-04 15:26:17 +0100

    Prima prova da Eclipse

---
+++ src/test1/classe1.java
@@ -0,0 +1,15 @@
+package test1;
+
+public class classe1 {
+
+	public classe1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("ciaoooo");
+		
+	}
+
+}
